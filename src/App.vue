c8d23e680737fab84862b52029291073924c0613
ndTh4ng <thangxacam@gmail.com>
2022-04-12 17:00:55 +0700

    style: style register form

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,62 @@
 <template>
   <div id="app">
     <nav>
-      <router-link to="/">TodoList</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/register">Register</router-link>
+      <router-link to="/">Home</router-link>
+      <div>
+        <router-link to="/login">Login</router-link>
+        <router-link to="/register">Register</router-link>
+      </div>
     </nav>
     <router-view />
   </div>
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  background-image: url("./assets/background.jpg");
+  position: fixed;
+  inset: 0;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
 }
 
 nav {
-  padding: 30px;
+  padding: 20px 40px;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 }
 
 nav a {
   font-weight: bold;
-  color: #2c3e50;
+  color: #eee;
+  text-decoration: none;
+  margin-right: 12px;
+}
+
+nav a:last-child {
+  margin-right: 0;
 }
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+input {
+  padding: 8px 10px;
+  border-radius: 5px;
+  border: none;
 }
 </style>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,66 +1,78 @@
 <template>
-  <div class="form">
-    <h1>This is Register page</h1>
-    <!-- The ValidationObserver offers a handleSubmit function that you can use to protect your form submissions.  -->
-    <ValidationObserver v-slot="{ handleSubmit }">
-      <form @submit.prevent="handleSubmit(onSubmit)">
-        <ValidationProvider
-          rules="required|min:4|max:128"
-          v-slot="{ errors }"
-          class="inputGroup"
-        >
-          <input v-model="name" placeholder="name" type="text" />
-          <span>{{ errors[0] }}</span>
-        </ValidationProvider>
+  <div class="container">
+    <div class="wrapper">
+      <h1>Registration</h1>
+      <!-- The ValidationObserver offers a handleSubmit function that you can use to protect your form submissions.  -->
+      <ValidationObserver v-slot="{ handleSubmit }">
+        <form @submit.prevent="handleSubmit(onSubmit)">
+          <ValidationProvider
+            rules="required|min:4|max:128"
+            v-slot="{ errors }"
+            class="inputGroup"
+          >
+            <input v-model="name" placeholder="Enter your name" type="text" />
+            <span>{{ errors[0] }}</span>
+          </ValidationProvider>
 
-        <ValidationProvider
-          rules="required"
-          v-slot="{ errors }"
-          class="inputGroup"
-        >
-          <input v-model="email" placeholder="email" type="email" />
-          <span>{{ errors[0] }}</span>
-        </ValidationProvider>
+          <ValidationProvider
+            rules="required"
+            v-slot="{ errors }"
+            class="inputGroup"
+          >
+            <input
+              v-model="email"
+              placeholder="Enter your email"
+              type="email"
+            />
+            <span>{{ errors[0] }}</span>
+          </ValidationProvider>
 
-        <ValidationProvider
-          rules="required|numberic|min-value:8"
-          v-slot="{ errors }"
-          class="inputGroup"
-          name="age"
-        >
-          <input v-model="age" placeholder="age" type="text" />
-          <span>{{ errors[0] }}</span>
-        </ValidationProvider>
+          <ValidationProvider
+            rules="required|numberic|min-value:8"
+            v-slot="{ errors }"
+            class="inputGroup"
+            name="age"
+          >
+            <input v-model="age" placeholder="Enter your age" type="text" />
+            <span>{{ errors[0] }}</span>
+          </ValidationProvider>
 
-        <ValidationProvider
-          rules="required|min:8"
-          v-slot="{ errors }"
-          vid="password"
-          class="inputGroup"
-        >
-          <input v-model="password" placeholder="password" type="text" />
-          <span>{{ errors[0] }}</span>
-        </ValidationProvider>
+          <ValidationProvider
+            rules="required|min:8"
+            v-slot="{ errors }"
+            vid="password"
+            class="inputGroup"
+          >
+            <input
+              v-model="password"
+              placeholder="Create password"
+              type="text"
+            />
+            <span>{{ errors[0] }}</span>
+          </ValidationProvider>
 
-        <ValidationProvider
-          rules="required|confirmed:password"
-          v-slot="{ errors }"
-          class="inputGroup"
-        >
-          <input
-            v-model="confirmPassword"
-            placeholder="confirm password"
-            type="text"
-          />
-          <span>{{ errors[0] }}</span>
-        </ValidationProvider>
+          <ValidationProvider
+            rules="required|confirmed:password"
+            v-slot="{ errors }"
+            class="inputGroup"
+          >
+            <input
+              v-model="confirmPassword"
+              placeholder="Confirm password"
+              type="text"
+            />
+            <span>{{ errors[0] }}</span>
+          </ValidationProvider>
 
-        <button type="submit">Submit</button>
-      </form>
-    </ValidationObserver>
-
-    <p v-if="user">{{ user.email }}</p>
-    <p v-if="token">{{ token }}</p>
+          <button type="submit">Register Now</button>
+        </form>
+      </ValidationObserver>
+      <div>
+        <p class="text-white">
+          Already have an account? <a href="/login">Login</a>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -141,15 +153,63 @@ extend("confirmed", {
 </script>
 
 <style scoped>
-.form {
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+h1 {
+  color: #eee;
+  margin-bottom: 12px;
+}
+.text-white {
+  color: #eee;
+}
+.wrapper {
+  padding: 32px 38px;
   margin: 0 auto;
-  max-width: 30%;
+  width: 400px;
   display: flex;
   flex-direction: column;
+  background-color: rgba(0, 0, 0, 0.6);
+  border-radius: 5px;
 }
 
 .inputGroup {
   display: flex;
   flex-direction: column;
+  margin-top: 12px;
+}
+
+span {
+  margin-top: 5px;
+  font-size: 0.75rem;
+  color: #ff0033;
+}
+
+button {
+  margin: 36px 0;
+  width: 100%;
+  border-radius: 4px;
+  padding: 8px;
+  background-color: #1a936f;
+  letter-spacing: 1px;
+  border: none;
+  color: #fff;
+  cursor: pointer;
+}
+a {
+  color: #1a936f;
+  font-weight: bold;
+}
+@media only screen and (max-width: 576px) {
+  .container {
+    padding: 0 20px;
+  }
+  .wrapper {
+    max-width: 100%;
+  }
 }
 </style>
